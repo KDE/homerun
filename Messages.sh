4172a1206da8c7e2275c08bbffbb9187a27f1feb
Aurélien Gâteau <agateau@kde.org>
2012-08-06 12:57:20 +0200

    Rename .po file so that it is loaded when running as a containment

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/homerun.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/plasma_applet_org.kde.homerun.pot
 rm -f rc.cpp
