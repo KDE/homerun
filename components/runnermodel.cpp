05001deba1b2301079b5b6a83f5eb37a46b9665a
Aurélien Gâteau <agateau@kde.org>
2012-11-13 14:45:45 +0100

    Give credit where credit is due

--- components/runnermodel.cpp
+++ components/runnermodel.cpp
@@ -1,21 +1,25 @@
 /*
- * Copyright 2012 Aurélien Gâteau <agateau@kde.org>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Library General Public License as
- * published by the Free Software Foundation; either version 2, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this program; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+Copyright 2012 Aurélien Gâteau <agateau@kde.org>
+
+Based on RunnerModel class. RunnerModel class copyright:
+
+Copyright 2011 Aaron Seigo <aseigo@kde.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+*/
 // Self
 #include <runnermodel.h>
 
