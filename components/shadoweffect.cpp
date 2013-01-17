3aa19f9f4540ae6114cac3ce2cc0714dddcaee56
Aurélien Gâteau <agateau@kde.org>
2013-01-17 17:43:10 +0100

    Bump copyright year

--- components/shadoweffect.cpp
+++ components/shadoweffect.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Aurélien Gâteau <agateau@kde.org>
+ * Copyright 2013 Aurélien Gâteau <agateau@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Library General Public License as
