bc86e3ae174e1f20db3717fa84630adb64f8a826
Aurélien Gâteau <agateau@kde.org>
2013-02-11 18:02:22 +0100

    First stab at a "single runner" source

---
+++ plugins/singlerunner/singlerunner.cpp
@@ -0,0 +1,176 @@
+/*
+Copyright 2013 Aurélien Gâteau <agateau@kde.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy
+defined in Section 14 of version 3 of the license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Self
+#include <singlerunner.h>
+
+// Homerun
+#include <abstractsource.h>
+#include <actionlist.h>
+
+// KDE
+#include <KDebug>
+#include <KLocale>
+#include <KPluginFactory>
+#include <Plasma/RunnerManager>
+
+// Qt
+#include <QIcon>
+
+typedef Homerun::SimpleSource<SingleRunnerModel> SingleRunnerSource;
+HOMERUN_EXPORT_SOURCE(singlerunner, SingleRunnerSource)
+
+SingleRunnerModel::SingleRunnerModel()
+: m_manager(0)
+{
+    QHash<int, QByteArray> roles;
+    roles.insert(Qt::DisplayRole, "display");
+    roles.insert(Qt::DecorationRole, "decoration");
+    roles.insert(HasActionListRole, "hasActionList");
+    roles.insert(ActionListRole, "actionList");
+    setRoleNames(roles);
+
+    init("desktopsessions");
+}
+
+void SingleRunnerModel::init(const QString& runnerId)
+{
+    if (!m_manager) {
+        m_manager = new Plasma::RunnerManager(this);
+        connect(m_manager, SIGNAL(matchesChanged(QList<Plasma::QueryMatch>)), SLOT(slotMatchesChanged(QList<Plasma::QueryMatch>)));
+    }
+    m_manager->setAllowedRunners(QStringList() << runnerId);
+    m_manager->setSingleModeRunnerId(runnerId);
+    m_manager->setSingleMode(true);
+    if (!m_manager->singleModeRunner()) {
+        kWarning() << "Failed to load runner" << runnerId << "as a single mode runner";
+        delete m_manager;
+        m_manager = 0;
+        return;
+    }
+    m_query = "$bypass-same-value-check";
+    setQuery(QString());
+}
+
+QString SingleRunnerModel::query() const
+{
+    return m_query;
+}
+
+void SingleRunnerModel::setQuery(const QString &value)
+{
+    if (m_query == value) {
+        return;
+    }
+    m_query = value;
+    QString term = prepareSearchTerm(m_query);
+    m_manager->launchQuery(term, m_manager->singleModeRunnerId());
+    queryChanged(m_query);
+}
+
+bool SingleRunnerModel::trigger(int row, const QString &actionId, const QVariant &actionArgument)
+{
+    return false;
+}
+
+QString SingleRunnerModel::name() const
+{
+    if (m_manager) {
+        return m_manager->singleModeRunner()->name();
+    } else {
+        return i18n("Single Runner");
+    }
+}
+
+int SingleRunnerModel::count() const
+{
+    return m_matches.count();
+}
+
+int SingleRunnerModel::rowCount(const QModelIndex& parent) const
+{
+    return parent.isValid() ? 0 : m_matches.count();
+}
+
+QVariant SingleRunnerModel::data(const QModelIndex& index, int role) const
+{
+    if (index.row() < 0 || index.row() >= m_matches.count()) {
+        return QVariant();
+    }
+    Plasma::QueryMatch match = m_matches.at(index.row());
+    if (role == Qt::DisplayRole) {
+        return match.text();
+    } else if (role == Qt::DecorationRole) {
+        return match.icon();
+    } else if (role == HasActionListRole) {
+        return false;
+    }
+    return QVariant();
+}
+
+QString SingleRunnerModel::prepareSearchTerm(const QString &term)
+{
+    const char *placeHolder = ":q:";
+    Q_ASSERT(m_manager);
+    Plasma::RunnerSyntax *syntax = m_manager->singleModeRunner()->defaultSyntax();
+    QStringList queries = syntax->exampleQueries();
+    Q_ASSERT(!queries.isEmpty());
+    QString query = queries.first();
+    if (query.contains(placeHolder)) {
+        return query.replace(placeHolder, term);
+    } else {
+        return query + ' ' + term;
+    }
+}
+
+void SingleRunnerModel::slotMatchesChanged(const QList< Plasma::QueryMatch > &matches)
+{
+    bool fullReset = false;
+    int oldCount = m_matches.count();
+    int newCount = matches.count();
+    if (newCount > oldCount) {
+        // We received more matches than we had. If all common matches are the
+        // same, we can just append new matches instead of resetting the whole
+        // model
+        for (int row = 0; row < oldCount; ++row) {
+            if (!(m_matches.at(row) == matches.at(row))) {
+                fullReset = true;
+                break;
+            }
+        }
+        if (!fullReset) {
+            // Not a full reset, inserting rows
+            beginInsertRows(QModelIndex(), oldCount, newCount);
+            m_matches = matches;
+            endInsertRows();
+            emit countChanged();
+        }
+    } else {
+        fullReset = true;
+    }
+
+    if (fullReset) {
+        beginResetModel();
+        m_matches = matches;
+        endResetModel();
+        emit countChanged();
+    }
+}
+
+#include <singlerunner.moc>
---
+++ plugins/singlerunner/singlerunner.h
@@ -0,0 +1,75 @@
+/*
+Copyright 2013 Aurélien Gâteau <agateau@kde.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy
+defined in Section 14 of version 3 of the license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef SINGLERUNNER_H
+#define SINGLERUNNER_H
+
+// KDE
+#include <QAbstractListModel>
+#include <Plasma/RunnerManager>
+
+/**
+ * This model exposes results from a runner with single-runner mode
+ */
+class SingleRunnerModel : public QAbstractListModel
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name CONSTANT)
+    Q_PROPERTY(int count READ count NOTIFY countChanged)
+    Q_PROPERTY(QString query READ query WRITE setQuery NOTIFY queryChanged)
+public:
+    SingleRunnerModel();
+
+    enum {
+        HasActionListRole = Qt::UserRole + 1,
+        ActionListRole,
+    };
+
+    Q_INVOKABLE bool trigger(int row, const QString &actionId, const QVariant &actionArgument);
+
+    QString name() const;
+
+    int count() const;
+
+    int rowCount(const QModelIndex &parent) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    void init(const QString &runnerId);
+
+    QString query() const;
+    void setQuery(const QString &query);
+
+Q_SIGNALS:
+    void countChanged();
+    void queryChanged(const QString &);
+
+private Q_SLOTS:
+    void slotMatchesChanged(const QList<Plasma::QueryMatch> &matches);
+
+private:
+    Plasma::RunnerManager *m_manager;
+
+    QString m_query;
+    QList<Plasma::QueryMatch> m_matches;
+
+    QString prepareSearchTerm(const QString &term);
+};
+
+#endif /* SINGLERUNNER_H */
