KDE/homerun

commit 3b311acb55850695b181008c8808508a30748a42

Sidebar width by (growth-only, for now) bounding box.

package/contents/ui/Page.qml
@@ -250,7 +250,15 @@ Item {
                     rightMargin: 10
                 }
 
-                opacity: model.checked ? 1 : (isContainment? 0.7 : 0.4)
+                onImplicitWidthChanged: {
+                    var fullWidth = Math.ceil(implicitWidth) + anchors.leftMargin + (anchors.rightMargin * 2);
+
+                    if (fullWidth > sideBarScrollArea.width) {
+                        sideBarScrollArea.width = fullWidth;
+                    }
+                }
+
+                opacity: model.checked ? 1 : (isContainment? 0.8 : 0.4)
 
                 text: model.display
                 font.pointSize: theme.defaultFont.pointSize * 1.4
@@ -350,7 +358,7 @@ Item {
 
         property int sideBarCount: 0
 
-        width: visible ? (theme.defaultFont.mSize.width * (isContainment ? 13 : 10)) * 1.4 : 0
+        width: 0
         opacity: visible ? 1 : 0
 
         visible: sideBarCount > 0

commit 2738f7ff2043ac0426b43c2350155191e9de91a8

Fix sidebar visibility by demand (regression from 3b311acb).

package/contents/ui/Page.qml
@@ -253,8 +253,8 @@ Item {
                 onImplicitWidthChanged: {
                     var fullWidth = Math.ceil(implicitWidth) + anchors.leftMargin + (anchors.rightMargin * 2);
 
-                    if (fullWidth > sideBarScrollArea.width) {
-                        sideBarScrollArea.width = fullWidth;
+                    if (fullWidth > sideBarScrollArea.textWidth) {
+                        sideBarScrollArea.textWidth = fullWidth;
                     }
                 }
 
@@ -357,8 +357,9 @@ Item {
         id: sideBarScrollArea
 
         property int sideBarCount: 0
+        property int textWidth: 0
 
-        width: 0
+        width: visible ? textWidth : 0
         opacity: visible ? 1 : 0
 
         visible: sideBarCount > 0