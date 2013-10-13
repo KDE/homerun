c16fb8d99a784e842de84860e49a2cbf4b591273
Eike Hein <hein@kde.org>
2013-10-14 00:25:00 +0200

    Another mirror sync test.

--- kicker/components/appletproxy.h
+++ kicker/components/appletproxy.h
@@ -16,8 +16,8 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef AppletProxy_H
-#define AppletProxy_H
+#ifndef APPLETPROXY_H
+#define APPLETPROXY_H
 
 #include <QObject>
 
