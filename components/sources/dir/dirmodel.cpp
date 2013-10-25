14d0797381302725f35e97e25166efc989b2fcab
Eike Hein <hein@kde.org>
2013-10-25 22:51:29 +0200

    Fix typo.

--- components/sources/dir/dirmodel.cpp
+++ components/sources/dir/dirmodel.cpp
@@ -192,7 +192,7 @@ bool DirModel::trigger(int row, const QString &actionId, const QVariant &actionA
         }
     }
 
-    ActionList::handleFileItemAction(item, actionId, actionArg, &close));
+    ActionList::handleFileItemAction(item, actionId, actionArg, &close);
 
     return false;
 }
